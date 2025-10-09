6829c42f62395051cc71a9132aed26accb064268
Carolina Sánchez <carolina.sanchezh02@gmail.com>
2025-10-08 22:30:07 -0600

    eccion 2

---
+++ Semana 4/seccion2.py
@@ -0,0 +1,24 @@
+"""nombre = input("ingrese su nombre: ")
+print("Hola", nombre)
+
+edad = int(input("ingrese su edad: "))
+print(edad*2)"""
+
+"""num1 = int(input("ingrese su primer número:"))
+num2 = int(input("ingrese su segundo número:"))
+print(num1 + num2)"""
+
+"""decimal= float(input("ingrese su bnúmero decimal: "))
+print(decimal/2)"""
+
+"""actual_year = 2025
+your_year = int(input("ingrese su año de nacimiento"))
+print(actual_year-your_year)"""
+
+"""uni_comprada = int(input("Ingrese las unidades de producto que compro:"))
+precio_prod = float(input("Ingrese el precio de producto: "))
+print(uni_comprada*precio_prod)"""
+
+nombre = input("Ingrese su nombre:")
+edad = input("Ingrese su edad:")
+input(f"Hola, {nombre}. Tienes {edad} años.")
