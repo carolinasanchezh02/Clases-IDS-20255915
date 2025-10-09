0cc0d9490da283945c5072a1c025267d32e02288
Carolina Sánchez <carolina.sanchezh02@gmail.com>
2025-10-08 21:00:35 -0600

    seccion1

---
+++ Semana 4/seccion1.py
@@ -0,0 +1,18 @@
+"""nombre = "Carolina"
+edad = 19  #Una variable sirve para crear un espacio que puede tener valores distintops a lo largo de todo el código, sin necesidad de estar modifican su nombre. Es decir, yo le asigno un nombre y puedo ir njugando con sus valores
+profesión = "estudiante"
+ciudad = "Lourdes"
+print(nombre)
+print(edad, profesión, ciudad)
+###############################################################
+
+bienvenida = "Bienvenido al curso de Python"
+print(bienvenida)"""
+
+#######################################################################
+anio_actual = 2025
+anio_nacimiento = 2006
+print(anio_actual-anio_nacimiento)
+"""La diferencia entre una cadena y un entero, es que la cadena puede procesar texto y distintos tipos de carcateres,
+mientras qu eun entero solo procesa numero enteros"""
+
