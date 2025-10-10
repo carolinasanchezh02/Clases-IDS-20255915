ac0121c495c267b42594c8249e3044b74292614d
Carolina Sánchez <carolina.sanchezh02@gmail.com>
2025-10-09 18:03:56 -0600

    ejercicios clase antes de control

---
+++ Semana 4/Semana5_/ejercciiocontrol.py
@@ -0,0 +1,63 @@
+"""Enero = int(input("Digite las cantidades de Enero: "))
+Febrero = int(input("Digite las cantidades de Febrero: "))
+Marzo = int(input("Digite las cantidades de Marzo: "))
+
+
+costo = Enero*1.2 + Febrero*1.38 + Marzo*1.14
+
+print(costo)
+print("Las cantidades de enero, febrero y marzo son:")
+print(f"{Enero}, {Febrero}, {Marzo} con un costo")
+print(f"de ${costo:.2f}")"""
+
+##2
+"""
+Días = ["lunes", "martes", "miércoles", "jueves", "viernes"] #Ojo con colocarle mayusculas y escribir igual el tetxo de aca de la lista con lo que coloco en el input
+Lu = int(input("Lunes: "))
+Días [0] = Lu
+print(Días)   #Para modificar el dato que esta dentro de la lista
+Mar = int(input("Martes: "))
+Días [1] = Mar
+print(Días)   #Para modificar el dato que esta dentro de la lista
+Mier = int(input("Miércoles: "))
+Días [2] = Mier
+print(Días)   #Para modificar el dato que esta dentro de la lista
+Jue = int(input("Jueves: "))
+Días [3] = Jue
+print(Días)   #Para modificar el dato que esta dentro de la lista
+Vie = int(input("Viernes: "))
+Días [4] = Vie
+print(Días)   #Para modificar el dato que esta dentro de la lista
+
+print(f"la producción de la semana fue {Lu+Mar+Mier+Jue+Vie}")"""
+
+
+### 4
+"""alumnos = ("Diego", "Paquito", "Calvito", "Aby", "Alvin", "Medranito", "Gene")
+alumnito = int(input("Ingrese el orden de niño que desea saber (1-7):"))
+print(f"El alumno que ingreso como numero {alumnito} es {alumnos[alumnito-1]}")"""
+
+##5
+"""nombre = input("Ingrese su primer nombre: ")
+apellido = input("Ingrese su apellido: ")
+dominio = "@isnd.com"
+
+propuesta1 = nombre.lower() + "."+ apellido.lower() + dominio
+propuesta2 = nombre[0].lower() + apellido.lower() + dominio
+
+print(propuesta1)
+print(propuesta2)
+#probar hacer con fstring
+"""
+
+###6
+"""monto = input("Ingrese su monto: ") #in para ver si esta o no esta
+print("$" in monto)
+print(monto[0] == "$")
+print(monto.count("$")==1)  #que me cuente que sea solo 1"""
+
+###7
+"""2 letras aleatorias"""
+letra1 = input("Ingrese su letra 1:")
+letra2 = input("Ingrese su letra 2:")
+
