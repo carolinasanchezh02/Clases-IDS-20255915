efa0ec9ef436a2a50243b916af2bf657d8802d4a
Carolina Sánchez <carolina.sanchezh02@gmail.com>
2025-10-16 20:45:29 -0600

    solucion de control 2
    ejer 1, 2 y 3

---
+++ Semana 4/Semana5_/Control 2_soluciones-py/ejer1.py
@@ -0,0 +1,3 @@
+menu = ("Hamburguesa", "Hotdog", "Pizza", "Tacos", "Lasaña", "Ensalada", "Pupusas", "Burrito", "Alitas de pollo", "Papas fritas")
+n = int(input())
+print("El plato seleccionado por Alvin es: " + menu[n-1])
---
+++ Semana 4/Semana5_/Control 2_soluciones-py/ejer2.py
@@ -0,0 +1,6 @@
+precios = [3.52, 55.15, 4.25, 60.25, 5.65, 3.15, 2.65, 70.75, 6.25, 2.55]
+número = int(input())
+nuevo_precio = float(input())
+precios[número-1]=nuevo_precio
+print(f"Los precios actualizados son: {precios}")
+
