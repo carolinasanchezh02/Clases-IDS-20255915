6d5a9eddce3e7ab5a0101a1494609c03a1276c20
Carolina Sánchez <carolina.sanchezh02@gmail.com>
2025-10-17 11:06:30 -0600

    solucion guia 2
    falta terminar ejercicio 7

--- Semana 4/Semana5_/ejercciiocontrol.py
+++ Semana 4/Semana5_/ejercciiocontrol.py
@@ -1,63 +1,76 @@
+###1
 """Enero = int(input("Digite las cantidades de Enero: "))
 Febrero = int(input("Digite las cantidades de Febrero: "))
 Marzo = int(input("Digite las cantidades de Marzo: "))
 
 
-costo = Enero*1.2 + Febrero*1.38 + Marzo*1.14
+costo = Enero*1.2 + Febrero*1.38 + Marzo*1.14"""
 
-print(costo)
-print("Las cantidades de enero, febrero y marzo son:")
-print(f"{Enero}, {Febrero}, {Marzo} con un costo")
-print(f"de ${costo:.2f}")"""
+#print(f"""La cantidad de artículos producidos fue: {Enero} en Enero, {Febrero} febrero y {Marzo} en Marzo. 
+#el costo total fue de ${costo: .2f}""")
 
 ##2
-"""
-Días = ["lunes", "martes", "miércoles", "jueves", "viernes"] #Ojo con colocarle mayusculas y escribir igual el tetxo de aca de la lista con lo que coloco en el input
+"""Días = ["lunes", "martes", "miércoles", "jueves", "viernes"] #Ojo con colocarle mayusculas y escribir igual el tetxo de aca de la lista con lo que coloco en el input
 Lu = int(input("Lunes: "))
 Días [0] = Lu
-print(Días)   #Para modificar el dato que esta dentro de la lista
+print(f"El día Lunes se vendieron {Lu} productos")   #Para modificar el dato que esta dentro de la lista
 Mar = int(input("Martes: "))
 Días [1] = Mar
-print(Días)   #Para modificar el dato que esta dentro de la lista
+print(f"El día Martes se vendieron {Mar} productos")   #Para modificar el dato que esta dentro de la lista
 Mier = int(input("Miércoles: "))
 Días [2] = Mier
-print(Días)   #Para modificar el dato que esta dentro de la lista
+print(f"El día Miércoles se vendieron {Mier} productos")   #Para modificar el dato que esta dentro de la lista
 Jue = int(input("Jueves: "))
 Días [3] = Jue
-print(Días)   #Para modificar el dato que esta dentro de la lista
+print(f"El día Jueves se vendieron {Jue} productos")   #Para modificar el dato que esta dentro de la lista
 Vie = int(input("Viernes: "))
 Días [4] = Vie
-print(Días)   #Para modificar el dato que esta dentro de la lista
+print(f"El día Viernes se vendieron {Vie} productos")#Para modificar el dato que esta dentro de la lista
+print(f"la producción de la semana fue {Lu+Mar+Mier+Jue+Vie}")
+print(f"Lista actualizada:{Días} ")"""
 
-print(f"la producción de la semana fue {Lu+Mar+Mier+Jue+Vie}")"""
-
+###3
+"""frutas = [1, 2, 3, 4, 5, 6]
+niño1 = input("¿Cuál es tu fruta fav?")
+frutas[0] = niño1
+print(frutas)
+niño1 = input("¿Cuál es tu fruta fav?")
+frutas[1] = niño1
+print(frutas)
+niño1 = input("¿Cuál es tu fruta fav?")
+frutas[2] = niño1
+print(frutas)
+niño1 = input("¿Cuál es tu fruta fav?")
+frutas[3] = niño1
+print(frutas)
+niño1 = input("¿Cuál es tu fruta fav?")
+frutas[4] = niño1
+print(frutas)
+niño1 = input("¿Cuál es tu fruta fav?")
+frutas[5] = niño1
+print(f"las frutas favoritas de los  niños son: {frutas}")"""
 
 ### 4
 """alumnos = ("Diego", "Paquito", "Calvito", "Aby", "Alvin", "Medranito", "Gene")
 alumnito = int(input("Ingrese el orden de niño que desea saber (1-7):"))
 print(f"El alumno que ingreso como numero {alumnito} es {alumnos[alumnito-1]}")"""
-
 ##5
-nombre = input("Ingrese su primer nombre: ")
+"""nombre = input("Ingrese su primer nombre: ")
 apellido = input("Ingrese su apellido: ")
 dominio = "@isnd.com"
 
-propuesta1 = nombre.lower() + "."+ apellido.lower() + dominio
-propuesta2 = nombre[0].lower() + apellido.lower() + dominio
+#propuesta1 = nombre.lower() + "."+ apellido.lower() + dominio
+#propuesta2 = nombre[0].lower() + apellido.lower() + dominio
 
-print(propuesta1)
-print(propuesta2)
-#probar hacer con fstring
-
+print(f"Propuesta 1: {nombre.lower()}.{apellido.lower()}{dominio}")
+print(f"Propuesta 2: {nombre[0].lower()}.{apellido.lower()}{dominio}")"""
 
 ###6
-"""monto = input("Ingrese su monto: ") #in para ver si esta o no esta
+monto = input("Ingrese su monto: ") #in para ver si esta o no esta
 print("$" in monto)
+print(monto.count("$")==1) 
 print(monto[0] == "$")
-print(monto.count("$")==1)  #que me cuente que sea solo 1"""
 
 ###7
-"""2 letras aleatorias
-letra1 = input("Ingrese su letra 1:")
-letra2 = input("Ingrese su letra 2:")"""
-
+#2 letras aleatorias
+palabra = "dfgupccbjkaj"
