e93147281ff52725d2c75dfaa3c48f9e42b20322
Carolina Sánchez <carolina.sanchezh02@gmail.com>
2025-10-18 10:59:31 -0600

    kk parcial

---
+++ Semana 4/Semana5_/Control 2_soluciones-py/parcial1/parcial1.py
@@ -0,0 +1,44 @@
+#Nombre: Carolina Michelle Sánchez Herrera
+#CARNET: 20255915
+
+"""###1
+num1 = int(input())
+num2 = int(input())
+print(num1 == num2)
+
+###2
+num1 = int(input())
+num2 = int(input())
+print(num1%num2 == 0)
+
+###3
+palabra = input().lower()
+letra = input().lower()
+print(palabra[-1]==letra)
+
+###4
+palabra = input().lower()
+print(palabra[:]==palabra[::-1])
+
+###5
+palabra = input().lower()
+letra = input().lower()
+print(letra in palabra)
+
+###6
+valor = int(input())
+print(type(valor) == int)"""
+
+###7
+DUI = input()
+val1 = len(DUI)== 10
+val2 = DUI[8] == "-"
+trans = int(DUI[-1])
+val3 = type(trans) is int
+
+print(val1 and val2 and val3)
+
+
+
+
+
